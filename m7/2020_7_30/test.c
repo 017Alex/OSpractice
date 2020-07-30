12259073a01e226c0fb1bb3800a3b807042d8a6e
wutong_017@163.com <wutong_017@163.com>
2020-07-30 22:31:07 +0800

    程序地址空间练习

--- m7/2020_7_30/test.c
+++ m7/2020_7_30/test.c
@@ -10,13 +10,22 @@ int main()
   char *str = "hello";
   printf("code addr: %p\n", main);
   printf("readonly : %p\n", str);
-  printf("init g_val: %p\n,", &g_val);
+  printf("init g_val: %p\n", &g_val);
   printf("un init g_val: %p\n", &un_g_val);
 
-  char *mem = (char *)malloc(10);
-  printf("heap addr: %p\n", mem);
+  char *mem1 = (char *)malloc(10);
+  char *mem2 = (char *)malloc(10);
+  char *mem3 = (char *)malloc(10);
+  printf("heap1 addr: %p\n", mem1);
+  printf("heap2 addr: %p\n", mem2);
+  printf("heap3 addr: %p\n", mem3);
 
+  static int x;
   printf("stack addr: %p\n", &str);
+  printf("stack addr: %p\n", &mem1);
+  printf("stack addr: %p\n", &mem2);
+  printf("stack addr: %p\n", &mem3);
+  printf("x stack addr: %p\n", &x);
 
   return 0;
 }
